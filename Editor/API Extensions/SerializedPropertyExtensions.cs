6983ae9a51187a55769cab1cb0220a4636dfefd7
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-21 15:31:31 +0100

    [FEAT] Added SerializedProperty.GetLabelContent() extension method.

---
+++ Editor/API Extensions/SerializedPropertyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class SerializedPropertyExtensions
+    {
+        public static GUIContent GetLabelContent(this SerializedProperty serializedProperty)
+        {
+            return new GUIContent(serializedProperty.displayName, serializedProperty.tooltip);
+        }
+    }
+}
