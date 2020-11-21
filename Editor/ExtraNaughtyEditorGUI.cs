50e05b43dddbed70339f88dbbf36833a1133b6eb
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-21 15:45:53 +0100

    [FEAT] Added ExtraNaughtyEditorGUI.Header() and ExtraNaughtyEditorGUILayout.Header()

---
+++ Editor/ExtraNaughtyEditorGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class ExtraNaughtyEditorGUI
+    {
+        public static void Header(Rect rect, string text)
+        {
+            EditorGUI.LabelField(rect, text, EditorStyles.boldLabel);
+        }
+    }
+}
---
+++ Editor/ExtraNaughtyEditorGUILayout.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class ExtraNaughtyEditorGUILayout
+    {
+        public static void Header(string text)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(text, EditorStyles.boldLabel);
+        }
+    }
+}
