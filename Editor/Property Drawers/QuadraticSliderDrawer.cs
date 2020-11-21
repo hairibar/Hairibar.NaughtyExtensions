97e743be6a44d5c14d0b71fe4def95a01888e6ee
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-21 16:00:14 +0100

    [FEAT] Added QuadraticSliderAttribute and QuadraticSliderDrawer

---
+++ Editor/Property Drawers/QuadraticSliderDrawer.cs
@@ -0,0 +1,28 @@
+﻿using NaughtyAttributes.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(QuadraticSliderAttribute))]
+    public class QuadraticSliderDrawer : PropertyDrawerBase
+    {
+        protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            QuadraticSliderAttribute quadraticAttribute = PropertyUtility.GetAttribute<QuadraticSliderAttribute>(property);
+            if (quadraticAttribute != null)
+            {
+                NonLinearSliderDrawer.Draw(rect, property, quadraticAttribute.Min, quadraticAttribute.Max, GetQuadraticFunction(quadraticAttribute.Power), label);
+            }
+        }
+
+        static NonLinearSliderDrawer.Function GetQuadraticFunction(float power)
+        {
+            return new NonLinearSliderDrawer.Function
+            {
+                function = (x) => Mathf.Pow(x, power),
+                backwardsFunction = (x) => Mathf.Pow(x, 1f / power)
+            };
+        }
+    }
+}
---
+++ Runtime/Drawer Attributes/QuadraticSliderAttribute.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes;
+
+namespace Hairibar.NaughtyExtensions
+{
+    public class QuadraticSliderAttribute : DrawerAttribute
+    {
+        public float Power { get; private set; }
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+
+        public QuadraticSliderAttribute(float min, float max, float power)
+        {
+            Min = min;
+            Max = max;
+            Power = power;
+        }
+    }
+}
