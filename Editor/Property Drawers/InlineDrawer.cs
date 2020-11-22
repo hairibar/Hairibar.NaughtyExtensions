f3146fe8f03c34fc3f48eb3522b3b815d8e19829
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 19:56:54 +0100

    [FEAT] Added InlineAttribute and InlineDrawer

---
+++ Editor/Property Drawers/InlineDrawer.cs
@@ -0,0 +1,57 @@
+﻿using NaughtyAttributes.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.NaughtyExtensions
+{
+    [CustomPropertyDrawer(typeof(InlineAttribute))]
+    public class InlineDrawer : PropertyDrawerBase
+    {
+        protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            if (property.hasChildren)
+            {
+                InlineAttribute attribute = PropertyUtility.GetAttribute<InlineAttribute>(property);
+
+                if (attribute.ShowHeaderAndBox)
+                {
+                    rect.position += new Vector2(0, EditorGUIUtility.singleLineHeight * 0.2f);
+
+                    EditorGUI.LabelField(rect, label, EditorStyles.boldLabel);
+                    NaughtyEditorGUI.BeginBoxGroup_Layout();
+                }
+
+                //Move to first child
+                SerializedProperty child = property.Copy();
+                child.Next(true);
+
+                SerializedProperty endProperty = property.GetEndProperty(true);
+
+                bool enterChildren;
+                if (attribute.ShowHeaderAndBox)
+                {
+                    enterChildren = EditorGUILayout.PropertyField(child);
+                }
+                else
+                {
+                    enterChildren = EditorGUI.PropertyField(rect, child);
+                }
+
+                while (child.Next(enterChildren) && !SerializedProperty.EqualContents(child, endProperty))
+                {
+                    enterChildren = EditorGUILayout.PropertyField(child);
+                }
+
+                if (attribute.ShowHeaderAndBox)
+                {
+                    NaughtyEditorGUI.EndBoxGroup_Layout();
+                }
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(property);
+                NaughtyEditorGUI.HelpBox_Layout("Can't use [Inline] on a property with no children.", MessageType.Warning);
+            }
+        }
+    }
+}
---
+++ Runtime/Drawer Attributes/InlineAttribute.cs
@@ -0,0 +1,16 @@
+﻿using NaughtyAttributes;
+
+namespace Hairibar.NaughtyExtensions
+{
+    public class InlineAttribute : DrawerAttribute
+    {
+        public bool ShowHeaderAndBox { get; private set; } = true;
+
+        public InlineAttribute() { }
+
+        public InlineAttribute(bool showHeaderAndBox)
+        {
+            ShowHeaderAndBox = showHeaderAndBox;
+        }
+    }
+}
