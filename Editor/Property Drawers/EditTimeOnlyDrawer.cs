acf24c014384773d9a264a2b7efabfed59469b5a
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 19:53:43 +0100

    [FEAT] Added EditTimeOnlyAttribute and EditTimeOnlyDrawer

---
+++ Editor/Property Drawers/EditTimeOnlyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(EditTimeOnlyAttribute))]
+    public class EditTimeOnlyDrawer : PropertyDrawerBase
+    {
+        protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            using (new EditorGUI.DisabledGroupScope(disabled: Application.isPlaying))
+            {
+                EditorGUI.PropertyField(rect, property, label, true);
+            }
+        }
+    }
+}
