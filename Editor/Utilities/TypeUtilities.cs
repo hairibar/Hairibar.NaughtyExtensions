70661862e1a15d7fd2dd1d3110fe4b4c7341202a
hairibar <aitor.iribar@gmail.com>
2021-01-05 18:38:14 +0100

    [FEAT] Added TypeUtilities

---
+++ Editor/Utilities/TypeUtilities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class TypeUtilities
+    {
+        public static System.Type FindType(string typeName, bool useFullName = false, bool ignoreCase = false)
+        {
+            if (string.IsNullOrEmpty(typeName)) return null;
+
+            bool isArray = typeName.EndsWith("[]");
+            if (isArray)
+                typeName = typeName.Substring(0, typeName.Length - 2);
+
+            StringComparison e = (ignoreCase) ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+            if (useFullName)
+            {
+                foreach (var assemb in System.AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    foreach (var t in assemb.GetTypes())
+                    {
+                        if (string.Equals(t.FullName, typeName, e))
+                        {
+                            if (isArray)
+                                return t.MakeArrayType();
+                            else
+                                return t;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var assemb in System.AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    foreach (var t in assemb.GetTypes())
+                    {
+                        if (string.Equals(t.Name, typeName, e) || string.Equals(t.FullName, typeName, e))
+                        {
+                            if (isArray)
+                                return t.MakeArrayType();
+                            else
+                                return t;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
