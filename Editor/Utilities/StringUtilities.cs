5f9c7b190b9ce5d84af30e5c963745b6c9b90c21
hairibar <aitor.iribar@gmail.com>
2021-01-05 18:36:51 +0100

    [FEAT] Added StringUtilities

---
+++ Editor/Utilities/StringUtilities.cs
@@ -0,0 +1,17 @@
+﻿namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class StringUtilities
+    {
+        public static string EnsureNotStartWith(this string value, string start)
+        {
+            if (value.StartsWith(start)) return value.Substring(start.Length);
+            else return value;
+        }
+
+        public static string EnsureNotEndsWith(this string value, string end)
+        {
+            if (value.EndsWith(end)) return value.Substring(0, value.Length - end.Length);
+            else return value;
+        }
+    }
+}
