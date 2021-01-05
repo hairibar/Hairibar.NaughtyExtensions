a663526bc2a810bb44ed02aea7aa048a87fa34ae
hairibar <aitor.iribar@gmail.com>
2021-01-05 18:38:31 +0100

    [FEAT] Added ReflectionUtilities

---
+++ Editor/Utilities/ReflectionUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace Hairibar.NaughtyExtensions.Editor
+{
+    public static class ReflectionUtilities
+    {
+        public static string GetAutoBackingFieldName(string propertyName)
+        {
+            return $"<{propertyName}>k__BackingField";
+        }
+
+        public static T[] GetEnumValues<T>() where T : System.Enum
+        {
+            return (T[]) System.Enum.GetValues(typeof(T));
+        }
+    }
+}
